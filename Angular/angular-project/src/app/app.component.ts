e38f2857532e1081a20548b11edda66712c1d1b8
imtiyaz-hirani <i.hirani@techskillsit.com>
2022-06-15 12:17:32 +0530

    interpolation + directives-ngIf,ngFor-click event

---
+++ Angular/angular-project/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  arr:number[] =[3,4,2,5,6,7,1];
+
+  names:string[]=['harry','ronald','hermione'];
+}
