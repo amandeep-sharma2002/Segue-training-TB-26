82b6589558bdcfc801f87b3a36d3975ce7afeee1
imtiyaz-hirani <i.hirani@techskillsit.com>
2022-06-20 12:18:47 +0530

    OnInit, classes and objects,filter

--- Angular/angular-project/src/app/app.component.ts
+++ Angular/angular-project/src/app/app.component.ts
@@ -1,13 +1,59 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {Product } from './product.model'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  product1: Product;
+  product2: Product;
+  product3: Product;
+  products: Product[];
+  category:string;
 
-  arr:number[] =[3,4,2,5,6,7,1];
+  constructor(){ }
 
-  names:string[]=['harry','ronald','hermione'];
+  ngOnInit(){
+
+    this.category = 'show-all';
+
+    this.product1={
+      pid:1,
+      pname: 'Oppo A12',
+      price: 12500,
+      categoryName: 'mobile'
+    };
+
+    this.product2={
+      pid:2,
+      pname: 'HP Laptop L8',
+      price: 53500,
+      categoryName: 'laptop'
+    };
+
+    this.product3={
+      pid:3,
+      pname: 'Apple IPhone 6',
+      price: 20500,
+      categoryName: 'mobile'
+    };
+
+    this.productArrayInit();
+  }
+
+  productArrayInit(){
+    this.products = [];
+    this.products.push(this.product1); //[p1]
+    this.products.push(this.product2); //[p1,p2]
+    this.products.push(this.product3); //[p1,p2,p3]
+  }
+
+  onCategorySelect(){
+    this.productArrayInit(); //[p1,p2,p3]
+
+    if(this.category != 'show-all')
+        this.products = this.products
+                    .filter(p=>p.categoryName == this.category);
+  }
 }
