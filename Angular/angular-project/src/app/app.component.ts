b1aae9c5d754e7183be0e12acc243e0f729de979
imtiyaz-hirani <i.hirani@techskillsit.com>
2022-06-21 11:56:02 +0530

    template based form with validation

--- Angular/angular-project/src/app/app.component.ts
+++ Angular/angular-project/src/app/app.component.ts
@@ -1,59 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import {Product } from './product.model'
+import { Component,   } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import {User} from './User.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  product1: Product;
-  product2: Product;
-  product3: Product;
-  products: Product[];
-  category:string;
+export class AppComponent {
+  user: User;
 
-  constructor(){ }
-
-  ngOnInit(){
-
-    this.category = 'show-all';
-
-    this.product1={
-      pid:1,
-      pname: 'Oppo A12',
-      price: 12500,
-      categoryName: 'mobile'
-    };
-
-    this.product2={
-      pid:2,
-      pname: 'HP Laptop L8',
-      price: 53500,
-      categoryName: 'laptop'
-    };
-
-    this.product3={
-      pid:3,
-      pname: 'Apple IPhone 6',
-      price: 20500,
-      categoryName: 'mobile'
-    };
-
-    this.productArrayInit();
-  }
-
-  productArrayInit(){
-    this.products = [];
-    this.products.push(this.product1); //[p1]
-    this.products.push(this.product2); //[p1,p2]
-    this.products.push(this.product3); //[p1,p2,p3]
-  }
-
-  onCategorySelect(){
-    this.productArrayInit(); //[p1,p2,p3]
-
-    if(this.category != 'show-all')
-        this.products = this.products
-                    .filter(p=>p.categoryName == this.category);
+  onFormSubmit(userForm:NgForm){
+      let nameVal = userForm.value.name;
+      let usernameVal = userForm.value.username;
+      let passwordVal = userForm.value.password;
+      this.user={
+        name: nameVal,
+        username: usernameVal,
+        password: passwordVal
+      }
+      console.log(this.user);
   }
 }
