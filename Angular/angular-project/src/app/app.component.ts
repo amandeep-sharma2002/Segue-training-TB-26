375e0209cb721a065426b4c1e6bd606909c7c9ae
imtiyaz-hirani <i.hirani@techskillsit.com>
2022-06-22 11:31:43 +0530

    case study:form+arrays+filter

--- Angular/angular-project/src/app/app.component.ts
+++ Angular/angular-project/src/app/app.component.ts
@@ -1,23 +1,42 @@
-import { Component,   } from '@angular/core';
+import { Component, OnInit,   } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import {User} from './User.model';
-@Component({
+import {Employee} from './employee.model';
+import { emp_data } from './data';
+ @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  user: User;
+export class AppComponent implements OnInit {
+  employee: Employee;
+  employees:Employee[];
+  devCount: number;
+  salesCount: number;
+  adminCount: number;
 
-  onFormSubmit(userForm:NgForm){
-      let nameVal = userForm.value.name;
-      let usernameVal = userForm.value.username;
-      let passwordVal = userForm.value.password;
-      this.user={
-        name: nameVal,
-        username: usernameVal,
-        password: passwordVal
-      }
-      console.log(this.user);
+  ngOnInit(){
+     this.employees =emp_data;
+     this.computeStat();
+  }
+
+  computeStat(){
+    this.devCount = this.employees.filter(e=> e.department =='development').length; //[e1];
+    this.salesCount = this.employees.filter(e=> e.department =='sales').length; //[e2];
+    this.adminCount = this.employees.filter(e=> e.department =='admin').length; //[e3];
+  }
+
+  onFormSubmit(employeeForm:NgForm){
+      let nameVal = employeeForm.value.name;
+      let salaryVal = employeeForm.value.salary;
+      let departmentVal = employeeForm.value.department;
+      /* Attach values to employee Object */
+       this.employee = {
+        name : nameVal,
+        salary : salaryVal,
+        department : departmentVal
+       }
+       /* Add employee object in array  */
+       this.employees.push(this.employee);
+       this.computeStat();
   }
 }
---
+++ Angular/angular-project/src/app/data.ts
@@ -0,0 +1,19 @@
+import { Employee } from "./employee.model";
+
+export const emp_data:Employee[] = [
+  {
+    name: 'harry potter',
+    salary: 85000,
+    department: 'development'
+  },
+  {
+    name: 'ronald weasley',
+    salary: 75000,
+    department: 'admin'
+  },
+  {
+    name: 'hermione granger',
+    salary: 95000,
+    department: 'sales'
+  }
+]
